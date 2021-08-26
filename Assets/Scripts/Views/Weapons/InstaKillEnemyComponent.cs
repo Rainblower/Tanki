bb90577fedffdedc6565b055b2dc6c2f7fdc7dd9
Rainblower <e.kolesnikov@doudougames.ru>
2021-08-26 23:29:26 +0300

    - New weapon

---
+++ Assets/Scripts/Views/Weapons/InstaKillEnemyComponent.cs
@@ -0,0 +1,21 @@
+﻿using Core;
+using Views.Weapons.Projectiles;
+
+namespace Views.Weapons
+{
+    public class InstaKillEnemyComponent: ProjectileWeapon
+    {
+        public override void Attack()
+        {
+            if (!CanAttack) return;
+            
+            var projectileFirst = BaseController.SystemController.GameController.ObjectsPooler.GetFromPool<BaseProjectile>(PoolingItem);
+            
+            projectileFirst.transform.position = ProjectileSpawnTransofm.position;
+            projectileFirst.Init(new ProjectileSettings{Direction = transform.up, Speed = Speed});
+            projectileFirst.Active(Damage);
+            
+            StartCountdown();
+        }
+    }
+}
